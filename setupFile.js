ee91ff459d95233d63ae242f1832b7873648a2f3
bruno <bruno@process.st>
2023-10-24 12:06:53 -0300

    remove comment

--- setupFile.js
+++ setupFile.js
@@ -1,5 +1,5 @@
-// setupFile.js <-- this will run before the tests in jest.
+// this will run before the tests in jest.
 import { setProjectAnnotations } from '@storybook/testing-react';
-import * as globalStorybookConfig from './.storybook/preview'; // path of your preview.js file
+import * as globalStorybookConfig from './.storybook/preview';
 
 setProjectAnnotations(globalStorybookConfig);
